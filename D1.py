0c0d542f510ec699c3c19b13c588c5dc46e8133d
crocmode <144585069+crocmode@users.noreply.github.com>
2025-07-29 14:54:52 +0530

    Initial commit

---
+++ D1.py
@@ -0,0 +1,67 @@
+# #print("Hello world")
+# dog="Tiger"
+# cat="Lion"
+# print("This is my dog's name:",dog)
+# print("This is my cat's name:",cat)
+# print("Hi", cat, "this is", dog) 
+
+# Arithmetic operations
+# num1=13
+# num2=13.2
+# name="Shreya"
+# value = True
+# Division ="a"
+# print(num1,num2,name,value,Division)
+# a=2
+# b=3
+# print("Sum:",a+b,"Mul:",a*b,"Subtract:",a-b,"Division:",a/b,"Modulus:",a%b)
+
+# #Assigning operation
+# a=10
+# print(a)
+# a+=2
+# a-=3
+# print(a)
+
+#Comparison operators
+# a=10
+# b=2
+# print(a==b)
+# print(a!=b)
+# print(a<b)
+# print(a>b)
+# print(a>=b)
+# print(a<=b)
+
+#Logical Operators
+# a=10
+# b=5
+# #and ya or ya not
+# if not(a>b or a!=b):
+#     print("Hello World")
+# else:
+#     print("No")
+
+# a=10
+# b=2
+# if (a==b):
+#     print("Hello World")
+# elif(a>b):
+#     print("Yes")
+# elif(a<b):
+#     print("No")
+# else:
+#     print("I am bored")
+
+#typecasting & input
+# a= int(input("Type your age: "))
+# print(a)
+# print(type(a))
+
+#Password Strength Calculator
+if (a==b):
+    print("Strong")
+elif(a>b):
+    print("Moderate")
+else:
+    print("Weak")
